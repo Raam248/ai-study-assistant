676439c3e6e0fa3b22d6adcda85e6420025457e8
Raam248 <raamprasad248@gmail.com>
2026-04-16 06:43:12 +0530

    added looping and json file to store data

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import api
+import memory
 def main():
-    chat = input("How You feeling today?")
-    response = api.get_response(chat)
-    print(response)
+    print("Welcome to the chatbot! How can I help you today?")
+    while True:
+     chat = input(">>> ")
+     if chat == "exit":
+        print("Thank you for using the chatbot! Goodbye!")
+        break
+     response = api.get_response(chat)
+     messages = memory.load_memory()
 
+     messages.append({"role": "user", "content": chat})
+     messages.append({"role": "assistant", "content": response})
+
+     memory.save_memory(messages)
+     print(">>>",response)
+    
 if __name__ == "__main__":
     main()
